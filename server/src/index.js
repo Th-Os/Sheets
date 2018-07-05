ad3f1a7e87aaec104c796a39d4c653be9de7104b
TrueCodingNinja <thomas.oswald@stud.uni-regensburg.de>
2018-07-05 14:34:53 +0200

    test api

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+import bp from 'body-parser';
+import express from 'express';
+import path from 'path';
+const app = express(),
+port = process.env.PORT || 3000;
+app.use(bp.json());
+app.use(bp.urlencoded({'extended': 'false'}));
+
+app.route('/')
+    .get(function(req, res) {
+        console.log(req)
+        res.send("hello")
+    })
+    .post(function(req, res) {
+        console.log(req)
+        res.send("bla")
+    });
+
+app.listen(port)
+
+export default app;
