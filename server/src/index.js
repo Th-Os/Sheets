911c34ce3f35fa31556decd56cef876ff280b6f5
TrueCodingNinja <thomas.oswald@stud.uni-regensburg.de>
2018-07-05 15:07:07 +0200

    print out port

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,21 @@
 import bp from 'body-parser';
 import express from 'express';
-const app = express(),
-port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(bp.json());
 app.use(bp.urlencoded({'extended': 'false'}));
 
 app.route('/')
-    .get(function(req, res) {
-        console.log(req)
-        res.send("hello")
-    })
-    .post(function(req, res) {
-        console.log(req)
-        res.send("bla")
-    });
+  .get(function (req, res) {
+    console.log(req);
+    res.send('hello');
+  })
+  .post(function (req, res) {
+    console.log(req);
+    res.send('bla');
+  });
 
-app.listen(port)
+app.listen(port);
+console.log('server is running at localhost:' + port);
 
 export default app;
