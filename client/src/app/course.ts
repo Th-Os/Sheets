45e766d5ad4bc26ccfb9c1935d3b79ef948748c4
Jo10 <Johannes.Wiesneth@stud.uni-regensburg.de>
2018-08-01 17:44:27 +0200

    Course management
    Create, edit, list and delete courses

---
+++ client/src/app/course.ts
@@ -0,0 +1,8 @@
+export class Course {
+  id: number;
+  name: string;
+  sheets: string[];
+  faculty: string;
+  semester: string;
+  min_req_sheets: number;
+}
---
+++ client/src/app/courses/courses.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseService} from "../course.service";
+import {Course} from "../course";
+import {MatDialog} from "@angular/material";
+import {CourseDialogComponent} from "../course-dialog/course-dialog.component";
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[];
+
+  constructor(
+    private courseService: CourseService,
+    public dialog: MatDialog) { }
+
+  ngOnInit() {
+    this.getCourses()
+  }
+
+  getCourses(): void {
+    this.courseService.getCourses()
+      .subscribe(courses => this.courses = courses);
+  }
+
+  create(): void {
+    const dialogRef = this.dialog.open(CourseDialogComponent, {
+      width: 'auto',
+      data: {create: true, course: null}
+    });
+
+    dialogRef.afterClosed().subscribe((course) => {
+      this.courseService.addCourse(course)
+        .subscribe(course => {
+          console.log(course);
+          this.courses.push(course);
+        });
+    });
+  }
+
+  update(course: Course): void {
+    const dialogRef = this.dialog.open(CourseDialogComponent, {
+      data: {create: false, course: course}
+    });
+
+    dialogRef.afterClosed().subscribe(updatedCourse => {
+      this.courseService.updateCourse(updatedCourse)
+        .subscribe(() => {
+          let index = this.courses.indexOf(this.courses.find((c) => c.id === course.id, course));
+          this.courses[index] = updatedCourse;
+        });
+    });
+  }
+
+  delete(course: Course): void {
+    this.courses = this.courses.filter(c => c !== course);
+    this.courseService.deleteCourse(course)
+  }
+}
