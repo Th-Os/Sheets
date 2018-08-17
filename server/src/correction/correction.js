edfe5cb46a46f97f9d366c1092e751a7bace90ba
TrueCodingNinja <thomas.oswald@stud.uni-regensburg.de>
2018-08-17 14:03:54 +0200

    implemented basic correction logic

--- server/src/correction/correction.js
+++ server/src/correction/correction.js
@@ -12,22 +12,64 @@ router.use(bodyParser.json());
 
 router.post('/', verify, function(req, res) {
     Submission.findById(req.body.submission.id).then(res => {
-        checkAnswerArray(res.answers);
+        beginCorrection(res.answers)
+            .then(() => { res.send('done'); })
+            .catch((err) => { res.status(400).send(err); });
     }).catch(err => {
         console.log(err);
+        res.status(400).send('error');
     });
     console.log('start auto correct');
-    res.send('auto correct started');
 });
 
-function checkAnswerArray(answers) {
+function beginCorrection(answers) {
+    return new Promise(checkAnswerArray(answers));
+}
+
+async function checkAnswerArray(answers) {
     for (let answer of answers) {
         console.log(answer);
         let task = answer.task;
         let solution = task.solution;
         console.log(task);
         console.log(solution);
+        await new Promise(checkAnswer(answer, solution, task));
     }
+    return 'done';
+}
+/**
+ * Returns achieved points or boolean indicating if true or false.
+ * @param {*} answer
+ * @param {*} solution
+ * @param {*} task
+ */
+function checkAnswer(answer, solution, task) {
+    let points = 0;
+    switch (solution.type) {
+        case 'none':
+            points = solution.default_points;
+            break;
+        case 'regex':
+            if (answer.text.match(solution.regex)) points = task.points;
+            break;
+        case 'range':
+            // could need parsing beforehand
+            if (answer.text >= solution.range.from && answer.text <= solution.range.to) points = task.points;
+            break;
+        case 'number':
+            // could need parsing beforehand
+            if (answer.text === solution.number) points = task.points;
+            break;
+        default:
+            throw Error('No specified Type found');
+    }
+
+    answer.set({achieved_points: points, auto_corrected: true});
+    answer.save(function(err, answer) {
+        if (err) throw err;
+        else return answer;
+    });
 }
 
+export {beginCorrection};
 export default router;
