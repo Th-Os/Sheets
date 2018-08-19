05ae67d1875a55c5fbf7d26f31534f8ef42ec085
TrueCodingNinja <thomas.oswald@stud.uni-regensburg.de>
2018-08-19 21:20:13 +0200

    refactored testing mechanism

--- server/src/correction/correction.js
+++ server/src/correction/correction.js
@@ -3,6 +3,7 @@ import verify from '../auth/verify';
 import bodyParser from 'body-parser';
 import {Submission} from '../models/submission';
 
+class CorrectionError extends Error {}
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({
@@ -12,31 +13,46 @@ router.use(bodyParser.json());
 
 router.post('/', verify, function(req, res) {
     Submission.findById(req.body.submission.id).then(res => {
-        beginCorrection(res.answers)
-            .then(() => { res.send('done'); })
-            .catch((err) => { res.status(400).send(err); });
+        beginCorrection(res.answers, function(err) {
+            if (err == null) res.status(200).send('done');
+            else res.status(400).send(err);
+        });
     }).catch(err => {
         console.log(err);
-        res.status(400).send('error');
+        res.status(400).send('not found');
     });
     console.log('start auto correct');
 });
 
-function beginCorrection(answers) {
-    return new Promise(checkAnswerArray(answers));
+function beginCorrection(answers, callback) {
+    try {
+        checkAnswerArray(answers, callback);
+    } catch (err) {
+        console.log('catched error before promises' + err.message);
+        callback(err);
+    }
 }
 
-async function checkAnswerArray(answers) {
+function checkAnswerArray(answers, callback) {
+    let promises = [];
     for (let answer of answers) {
-        console.log(answer);
         let task = answer.task;
         let solution = task.solution;
-        console.log(task);
-        console.log(solution);
-        await new Promise(checkAnswer(answer, solution, task));
+        promises.push(new Promise(function(resolve, reject) {
+            try {
+                checkAnswer(answer, solution, task);
+                resolve();
+            } catch (err) {
+                reject(err);
+            }
+        }));
     }
-    return 'done';
+    Promise.all(promises).then((answer) => {
+        console.log(answer);
+        callback();
+    }).catch((err) => { callback(err); });
 }
+
 /**
  * Returns achieved points or boolean indicating if true or false.
  * @param {*} answer
@@ -51,14 +67,16 @@ function checkAnswer(answer, solution, task) {
             break;
         case 'regex':
             if (answer.text.match(solution.regex)) points = task.points;
+            else throw new CorrectionError('regex "' + solution.regex + '" does not match "' + answer.text + '"');
             break;
         case 'range':
-            // could need parsing beforehand
-            if (answer.text >= solution.range.from && answer.text <= solution.range.to) points = task.points;
+            let value = Number(answer.text);
+            if (value >= solution.range.from && value <= solution.range.to) points = task.points;
+            else throw new CorrectionError('range "' + solution.range + '" does not match "' + answer.text + '"');
             break;
         case 'number':
-            // could need parsing beforehand
-            if (answer.text === solution.number) points = task.points;
+            if (answer.text === solution.number.toString()) points = task.points;
+            else throw new CorrectionError('number "' + solution.number + '" does not match "' + answer.text + '"');
             break;
         default:
             throw Error('No specified Type found');
--- server/test/auto_correction_test.js
+++ server/test/auto_correction_test.js
@@ -1,11 +1,16 @@
+/* eslint-env mocha */
+
 import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import * as correction from '../src/correction/correction';
 import {Submission, Answer, Student} from '../src/models/submission';
 import {Task, Solution} from '../src/models/sheet';
 
+chai.use(chaiAsPromised).should();
+
 let submission = new Submission({student: new Student({name: 'bla', mat_nr: 123}),
     answers: [new Answer({
-        text: '2',
+        text: '3',
         task: new Task({
             question: 'number task',
             points: 10,
@@ -17,17 +22,49 @@ let submission = new Submission({student: new Student({name: 'bla', mat_nr: 123}
             })
         })
     })]});
-let subId = 0;
-chai.assert.isNotNull(submission, 'submission is not null');
-submission.save(function(err, sub, number) {
-    if (err) throw err;
-    subId = sub.id;
-    console.log('saved submission with id: ' + subId);
-    correction.beginCorrection(submission.answers).then(() => {
-        Submission.findById(subId).then((sub) => {
-            let number = sub.answers[0];
-            chai.assert.isTrue(number.auto_corrected, 'Answer was auto-corrected');
-            chai.assert.strictEqual(number.achieved_points, 10, 'Achieved points are correct');
-        }).catch((err) => { throw err; });
-    }).catch((err) => { throw err; });
+
+it('should not be null', function() {
+    chai.assert.isNotNull(submission, 'submission is not null');
 });
+
+let error;
+it('correction is executed', function(done) {
+    correction.beginCorrection(submission.answers, function(err) {
+        error = err;
+        done();
+    });
+});
+
+it('correction has error', function() {
+    chai.assert.exists(error);
+    chai.assert.instanceOf(error, Error, 'Error');
+});
+
+it('submission was auto-corrected', function() {
+    chai.assert.exists(submission.answers[0].auto_corrected);
+    chai.assert.isTrue(submission.answers[0].auto_corrected);
+});
+
+/*
+it('should begin correction', function(done) {
+    correction.beginCorrection(submission.answers).then(() => {
+        done();
+        it('should find submission', function(done) {
+            Submission.findById(subId).then((sub) => {
+                done();
+                let number = sub.answers[0];
+                describe('Answer correction', function() {
+                    it('should be auto-corrected', function(done) {
+                        chai.assert.isTrue(number.auto_corrected, 'Answer was auto-corrected');
+                        done();
+                    });
+                    it('achieved points should be correct', function(done) {
+                        chai.assert.strictEqual(number.achieved_points, 10, 'Achieved points are correct');
+                        done();
+                    });
+                });
+            }).catch((err) => { throw err; });
+        }).catch((err) => { throw err; });
+    });
+});
+*/
