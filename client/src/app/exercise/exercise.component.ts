6e2559e4cf5a2d77507a66c5cba856585db4bc48
M-Schenk-91 <schenk_matthias@web.de>
2018-08-23 15:26:02 +0200

    exercisepage parsing files to upload started

--- client/src/app/exercise/exercise.component.ts
+++ client/src/app/exercise/exercise.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as JSZip from 'jszip';
 
 @Component({
 	selector: 'app-exercise',
@@ -10,8 +11,54 @@ export class ExerciseComponent implements OnInit {
 	selectedFile = null;
 	dropzoneActive:boolean = false;
 
+	constructor() { }
+
+	ngOnInit() {
+
+	}
+
 	onFilesAdded(fileList: FileList){
-		console.log(fileList);
+		if(fileList.length <= 0){
+			console.log("Error reading files: List <= 0")
+			return
+		}
+
+		if(fileList.length == 1 && (fileList[0].type == "application/zip" || fileList[0].type =="application/octet-stream" || fileList[0].type =="application/x-zip-compressed" || fileList[0].type =="multipart/x-zip")){
+			this.addZipFolder(fileList[0]);
+			return
+		}
+
+		//TODO: add functionality for multiple single files
+		//this.addSingleFiles(fileList);
+	}
+
+	addSingleFiles(fileList){
+
+	}
+
+	addZipFolder(file){
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			var zip = new JSZip();
+			zip.loadAsync(file).then(function (zip) {
+				console.log(zip)
+				Object.keys(zip.files).forEach(function (filename) {
+
+					//console.log(filename)    
+
+
+					//zip.files[filename].async('string').then(function (fileData) {
+      					//console.log(fileData) // These are your file contents  
+  					//})
+				})
+			})
+		}
+
+		reader.readAsArrayBuffer(file);
+	}
+
+	addFile(file){
+
 	}
 
 	handleFileSelection(event){
@@ -22,16 +69,7 @@ export class ExerciseComponent implements OnInit {
 		this.onFilesAdded(fileList);
 	}
 
-
-
 	dropzoneState($event: boolean) {
 		this.dropzoneActive = $event;
 	}
-
-	constructor() { }
-
-	ngOnInit() {
-
-	}
-
 }
