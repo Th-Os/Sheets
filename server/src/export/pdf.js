d32e5ae6e7acc8fdd1884707d12ef81edf94a005
TrueCodingNinja <thomas.oswald@stud.uni-regensburg.de>
2018-08-26 00:22:50 +0200

    init jsreport library

---
+++ server/src/export/pdf.js
@@ -0,0 +1,47 @@
+import report from 'jsreport-core';
+import fs from 'fs';
+
+function PDFRenderer() {
+    this.pdf = {
+        template: {
+            content: 'localhost:3000/resources/pdf.html',
+            engine: 'handlebars',
+            recipe: 'chrome-pdf'
+        },
+        data: {},
+        name: 'abc'
+    };
+    this.renderer = report();
+}
+
+PDFRenderer.prototype.html = function(html) {
+    this.pdf.template.content = html;
+    return this;
+};
+
+PDFRenderer.prototype.data = function(data) {
+    this.pdf.data = data;
+    return this;
+};
+
+PDFRenderer.prototype.name = function(name) {
+    this.pdf.name = name;
+    return this;
+};
+
+PDFRenderer.prototype.send = function(res) {
+    this.renderer.init().then(() => {
+        this.renderer.render({
+            template: this.pdf.template,
+            data: this.pdf.data
+        }).then((response) => {
+            // TODO send via buffer or stream
+            fs.writeFileSync('output/' + this.pdf.name + '.pdf', response.content);
+            res.download('output/' + this.pdf.name + '.pdf');
+        });
+    }).catch((e) => {
+        console.log(e);
+    });
+};
+
+export default PDFRenderer;
--- server/src/routes/export.js
+++ server/src/routes/export.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 import verify from '../auth/verify';
+import PDF from '../export/pdf';
 import {Sheet} from '../models/sheet';
 
 const router = express.Router();
 
 router.get('/pdf/:id', verify, function(req, res) {
-    res.send('not implemented yet');
+    fs.readFile(path.join(__dirname, '../../resources/pdf.html'), 'utf8', function(err, html) {
+        if (err) res.status(400).send(err);
+        new PDF().html(html).send(res);
+    });
 });
 
 router.get('/word/:id', verify, function(req, res) {
