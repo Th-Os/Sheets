3a4d93bb8928616ead8418587315938a2f71e88a
TrueCodingNinja <thomas.oswald@stud.uni-regensburg.de>
2018-08-26 20:08:53 +0200

    Trying out with data

--- server/src/routes/export.js
+++ server/src/routes/export.js
@@ -7,10 +7,58 @@ import {Sheet} from '../models/sheet';
 
 const router = express.Router();
 
+const sheet =
+        {
+            name: 'Exercise Sheet',
+            submissiondate: '2016-05-18 10:00:00.000',
+            min_req_points: 10,
+            submissions: [],
+            exercises: [
+                {
+                    name: 'Übung 1',
+                    description: 'Aufgabenbeschreibung',
+                    tasks: [
+                        {
+                            question: 'Frage',
+                            points: 10,
+                            order: 1,
+                            choices: '1|2|3'
+                        },
+                        {
+                            question: 'Frage',
+                            points: 10,
+                            order: 2,
+                            choices: '1|2|3'
+                        }
+                    ],
+                    order: 1
+                },
+                {
+                    name: 'Übung 2',
+                    description: 'Aufgabenbeschreibung',
+                    tasks: [
+                        {
+                            question: 'Frage',
+                            points: 10,
+                            order: 1,
+                            choices: '1|2|3'
+                        },
+                        {
+                            question: 'Frage',
+                            points: 10,
+                            order: 2,
+                            choices: '1|2|3'
+                        }
+                    ],
+                    order: 1
+                }
+            ]
+        };
+
 router.get('/pdf/:id', verify, function(req, res) {
     fs.readFile(path.join(__dirname, '../../resources/pdf.html'), 'utf8', function(err, html) {
         if (err) res.status(400).send(err);
-        new PDF().html(html).send(res);
+        new PDF().data(sheet).html(html).send(res);
     });
 });
 
