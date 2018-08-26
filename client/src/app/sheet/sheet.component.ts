66133c40125070562ba9f7304f8fa934c52045f3
M-Schenk-91 <schenk_matthias@web.de>
2018-08-26 17:02:24 +0200

    read submissions

--- client/src/app/sheet/sheet.component.ts
+++ client/src/app/sheet/sheet.component.ts
@@ -4,7 +4,12 @@ import {CourseService} from "../course.service";
 import {SheetService} from "../sheet.service";
 import {Location} from "@angular/common";
 import {Sheet} from "../sheet";
+import {Submission} from "../submission";
+import {Student} from "../student";
+import {Answer} from "../answer";
 import * as JSZip from 'jszip';
+//Achtung: Nach npm install muss im File "client/node_modules/jszip/lib/readable-stream-browser.js" die Zeile "module.exports = require("stream");" 
+//durch "module.exports = require("readable-stream");" ersetzt werden!
 
 @Component({
   selector: 'app-sheet',
@@ -22,7 +27,7 @@ export class SheetComponent implements OnInit {
     private route: ActivatedRoute,
     private sheetService: SheetService,
     private location: Location
-  ) { }
+    ) { }
 
   ngOnInit() {
     this.getSheet();
@@ -31,13 +36,16 @@ export class SheetComponent implements OnInit {
   getSheet(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.sheetService.getSheet(id);
+    //TODO
+    this.sheet = new Sheet();
+    this.sheet.submissions = [];
   }
 
   goBack(): void {
     this.location.back();
   }
 
-    onFilesAdded(fileList: FileList){
+  onFilesAdded(fileList: FileList): void {
     if(fileList.length <= 0){
       console.log("Error reading files: List <= 0")
       return
@@ -47,28 +55,38 @@ export class SheetComponent implements OnInit {
       this.addZipFolder(fileList[0]);
       return
     }
-
-    //TODO: add functionality for multiple single files
-    //this.addSingleFiles(fileList);
+    //TODO: add functionality for multiple files
   }
 
-  addSingleFiles(fileList){
-
-  }
-
-  addZipFolder(file){
+  addZipFolder(file): void {
     var reader = new FileReader();
-    reader.onload = function (e) {
+    reader.onload = (e) => {
       var zip = new JSZip();
-      zip.loadAsync(file).then(function (zip) {
-        Object.keys(zip.files).forEach(function (filename) {
+      zip.loadAsync(file).then((zip) => {
+        let counter: number = 0;
+        Object.keys(zip.files).forEach((filename) => {
+          if(filename.split("/").length < 3) return;
+          if(!filename.includes(".txt")) {
+            counter++;
+            let submission = new Submission();
+            let student = new Student();
 
-          console.log(filename)    
+            student.name = this.getStudentName(filename);
+            student.id = counter;
+            submission.id = counter;
+            submission.student_id = student.id;
+            submission.name = student.name;
+
+            this.sheet.submissions.push(submission);
+          }
 
 
-          //zip.files[filename].async('string').then(function (fileData) {
-                //console.log(fileData) // These are your file contents  
-            //})
+          zip.files[filename].async('string').then((fileData) => {
+                //student.id = getStudentId(filename);
+                let answer = new Answer(); 
+                //answer.text = getAnswerText(fileData);
+                //console.log(fileData)
+              })
         })
       })
     }
@@ -76,20 +94,31 @@ export class SheetComponent implements OnInit {
     reader.readAsArrayBuffer(file);
   }
 
-  addFile(file){
+  getStudentName(fileName): string {
+    let res = null;
+    let pathSlices = fileName.split("/");
+
+    if(pathSlices.length < 2) return null;
+
+    //"Vorname0 Nachname0_1327627_assignsubmission_file_"
+    let relevantFolderName: string = pathSlices[pathSlices.length - 2]; 
+    res = relevantFolderName.split("_")[0];
+    return res;
+  }
+
+  addFile(file): void {
 
   }
 
-  handleFileSelection(event){
+  handleFileSelection(event): void {
     this.onFilesAdded(event.target.files);
   }
 
-  handleFileDrop(fileList: FileList) {
+  handleFileDrop(fileList: FileList): void {
     this.onFilesAdded(fileList);
   }
 
-  dropzoneState($event: boolean) {
+  dropzoneState($event: boolean): void {
     this.dropzoneActive = $event;
   }
-
 }
---
+++ client/src/app/submission.ts
@@ -0,0 +1,8 @@
+export class Submission {
+	id: number;
+	user_id: number;
+	student_id: number;
+	answer: string;
+	//----
+	name:string;
+}
