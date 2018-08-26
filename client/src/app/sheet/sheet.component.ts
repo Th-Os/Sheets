40f17cc84ae680191e006f7cfcefb2ce2d0b570f
M-Schenk-91 <schenk_matthias@web.de>
2018-08-26 15:07:17 +0200

    refactoring, exercise component removed
    Achtung: Nach npm install muss im File "client/node_modules/jszip/lib/readable-stream-browser.js" die Zeile "module.exports = require("stream");" durch "module.exports = require("readable-stream");" ersetzt werden!

--- client/src/app/sheet/sheet.component.ts
+++ client/src/app/sheet/sheet.component.ts
@@ -4,6 +4,7 @@ import {CourseService} from "../course.service";
 import {SheetService} from "../sheet.service";
 import {Location} from "@angular/common";
 import {Sheet} from "../sheet";
+import * as JSZip from 'jszip';
 
 @Component({
   selector: 'app-sheet',
@@ -13,6 +14,9 @@ import {Sheet} from "../sheet";
 export class SheetComponent implements OnInit {
 
   sheet: Sheet;
+
+  selectedFile = null;
+  dropzoneActive:boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -31,6 +35,61 @@ export class SheetComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+    onFilesAdded(fileList: FileList){
+    if(fileList.length <= 0){
+      console.log("Error reading files: List <= 0")
+      return
+    }
+
+    if(fileList.length == 1 && (fileList[0].type == "application/zip" || fileList[0].type =="application/octet-stream" || fileList[0].type =="application/x-zip-compressed" || fileList[0].type =="multipart/x-zip")){
+      this.addZipFolder(fileList[0]);
+      return
+    }
+
+    //TODO: add functionality for multiple single files
+    //this.addSingleFiles(fileList);
+  }
+
+  addSingleFiles(fileList){
+
+  }
+
+  addZipFolder(file){
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      var zip = new JSZip();
+      zip.loadAsync(file).then(function (zip) {
+        Object.keys(zip.files).forEach(function (filename) {
+
+          console.log(filename)    
+
+
+          //zip.files[filename].async('string').then(function (fileData) {
+                //console.log(fileData) // These are your file contents  
+            //})
+        })
+      })
+    }
+
+    reader.readAsArrayBuffer(file);
+  }
+
+  addFile(file){
+
+  }
+
+  handleFileSelection(event){
+    this.onFilesAdded(event.target.files);
+  }
+
+  handleFileDrop(fileList: FileList) {
+    this.onFilesAdded(fileList);
+  }
+
+  dropzoneState($event: boolean) {
+    this.dropzoneActive = $event;
   }
 
 }
